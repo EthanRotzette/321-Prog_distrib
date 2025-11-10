f3a2a3a967f38becb58e362329ec3dc96600a843
Ethan Rotzetter <167179837+EthanRotzette@users.noreply.github.com>
2025-11-10 11:25:23 +0100

    feat(serial) étape 4 et 5 terminé

--- exos_fait/Serial/Serial/Program.cs
+++ exos_fait/Serial/Serial/Program.cs
@@ -1,37 +1,45 @@
-﻿
-using System.Text.Json;
+﻿using System.Text.Json;
 
-Actor Actor = new Actor
+Episode episode = new Episode
 {
-    FirstName = "Jean",
-    LastName = "Dujardin",
-    BirthDate = new DateTime(2000, 11, 5),
-    Country = "France",
-    IsAlive = true
+    Title = "La petite maison dans la prairie",
+    DurationMinutes = 35,
+    SequenceNumber = 12,
+    Director = "Orson Krennic",
+    Synopsis = "René par à la recherche de son chien perdu dans la forêt",
+    Characters = new List<Character>
+    {
+        new Character
+        {
+            FirstName = "René",
+            LastName = "Bernard",
+            Description = "Personnage secondaire de la série",
+            PlayedBy = new Actor
+            {
+                FirstName = "Jean",
+                LastName = "Dujardin",
+                BirthDate = new DateTime(2000, 11, 5),
+                Country = "France",
+                IsAlive = true
+            }
+        }
+    }
 };
 
-Character character = new Character 
-{
-    FirstName = "René",
-    LastName = "Bernard",
-    Description = "Personnage secondaire de la série",
-    PlayedBy = Actor
-};
+string jsonEpisode = JsonSerializer.Serialize(episode);
+File.WriteAllText("C:\\Users\\pk50gbi\\Documents\\GitHub\\321-Prog_distrib\\exos_fait\\Serial\\episode.json", jsonEpisode);
 
-string jsonCharacter = JsonSerializer.Serialize(character);
+string jsonCharacter = JsonSerializer.Serialize(episode.Characters[0]);
 File.WriteAllText("C:\\Users\\pk50gbi\\Documents\\GitHub\\321-Prog_distrib\\exos_fait\\Serial\\rene.json", jsonCharacter);
 
-string jsonActor = JsonSerializer.Serialize(Actor);
+string jsonActor = JsonSerializer.Serialize(episode.Characters[0].PlayedBy);
 File.WriteAllText("C:\\Users\\pk50gbi\\Documents\\GitHub\\321-Prog_distrib\\exos_fait\\Serial\\Dujardin.json", jsonActor);
 
-string jsonEliottin = File.ReadAllText("C:\\Users\\pk50gbi\\Documents\\GitHub\\321-Prog_distrib\\exos_fait\\Serial\\eliottin.json");
 string jsonRene = File.ReadAllText("C:\\Users\\pk50gbi\\Documents\\GitHub\\321-Prog_distrib\\exos_fait\\Serial\\rene.json");
 
-Character Eliottin = JsonSerializer.Deserialize<Character>(jsonEliottin);
 Character perso = JsonSerializer.Deserialize<Character>(jsonRene);
 
 Console.WriteLine($"Le personnage de {perso.FirstName} {perso.LastName} est joué par {perso.PlayedBy.FirstName} {perso.PlayedBy.LastName}");
-
 
 public class Character
 {
@@ -48,4 +56,14 @@ public class Actor
     public DateTime BirthDate { get; set; }
     public string Country { get; set; }
     public bool IsAlive { get; set; }
+}
+
+public class Episode
+{
+    public string Title { get; set; }
+    public int DurationMinutes { get; set; }
+    public int SequenceNumber { get; set; }
+    public string Director { get; set; }
+    public string Synopsis { get; set; }
+    public List<Character> Characters { get; set; } = new List<Character>();
 }
